dd2c7e405b4b60dc259abe98b693ce9fe940d75e
Ahmed11711 <ahmed11711@gmail.com>
2026-03-18 16:50:09 +0200

    test2

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('test', function () {
+Route::get('test22', function () {
     return response()->json(['message' => 'API is working']);
 });
 Route::prefix('v1/auth')->group(function () {
