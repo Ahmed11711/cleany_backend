b5d6bff44c2d9443d4e4df2c2b274056bf6726cc
Ahmed11711 <ahmed11711@gmail.com>
2026-03-26 13:58:12 +0200

    aa

--- app/Http/Controllers/API/Transaction/TransactionController.php
+++ app/Http/Controllers/API/Transaction/TransactionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Transaction;
 
 use App\Http\Controllers\Controller;
+use App\Models\booking;
 use App\Models\Transaction;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
@@ -12,8 +13,8 @@ class TransactionController extends Controller
     use ApiResponseTrait;
     public function index(Request $request)
     {
-        return   $userId = $request->user_id;
-        $transactions = Transaction::where('user_id', $userId)->get();
+        $userId = $request->user_id;
+        $transactions = booking::where('user_id', $userId)->get();
         return $this->successResponse($transactions, "My Transaction List");
     }
 }
