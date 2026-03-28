4e5b63eac2d6b5bc940fcdd66c5705170cc9302d
Ahmed11711 <ahmed11711@gmail.com>
2026-03-28 09:57:52 +0200

    aa

--- app/Http/Resources/Admin/Offer/OfferResource.php
+++ app/Http/Resources/Admin/Offer/OfferResource.php
@@ -15,7 +15,13 @@ class OfferResource extends JsonResource
             'is_active' => $this->is_active,
             'image_path' => $this->image_path,
             'company_id' => $this->company_id,
+            'company_name' => $this->whenLoaded('company', function () {
+                return $this->company->name;
+            }),
             'category_id' => $this->category_id,
+            'category_name' => $this->whenLoaded('category', function () {
+                return $this->category->name;
+            }),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
