94094364f3ee8584a3b31d4453372db888df401d
Ahmed11711 <ahmed11711@gmail.com>
2026-03-29 07:06:08 +0200

    ahmed

--- app/Http/Resources/Api/Transaction/AllTransactionResource.php
+++ app/Http/Resources/Api/Transaction/AllTransactionResource.php
@@ -15,11 +15,19 @@ class AllTransactionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'service_id' => $this->service_id,
-            'service_name' => $this->service->service_name ?? null,
-            'created_at'   => $this->created_at,
-
+            'id'             => $this->id,
+            'transaction_id' => $this->transaction_id,
+            'order_id'       => $this->order_id,
+            'amount'         => $this->amount,
+            'type'           => $this->type,
+            'payment_method' => $this->payment_method,
+            'status'         => $this->status,
+            'notes'          => $this->notes,
+            'service_id'     => $this->service_id,
+            // Accessing the relationship safely
+            'service_name'   => $this->service->service_name ?? "Deposite",
+            'created_at'     => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at'     => $this->updated_at->format('Y-m-d H:i:s'),
         ];
     }
 }
