13eb6d60bc408fcff0a41ccabda559562a85f7ea
Ahmed11711 <ahmed11711@gmail.com>
2026-03-29 07:11:20 +0200

    ahmed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\Company\Checkout\CheckoutController;
-use App\Http\Controllers\Api\Payment\CreateLinkPaymentController;
+use App\Http\Controllers\API\Payment\CreateLinkPaymentController;
 use Illuminate\Support\Facades\Route;
 
 
