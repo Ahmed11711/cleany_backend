f002f200282b4d6e3ea344543d7ac36661ffc72c
Ahmed11711 <ahmed11711@gmail.com>
2026-03-30 23:17:38 +0200

    ss

--- app/Http/Requests/Api/Favou/FavRequest.php
+++ app/Http/Requests/Api/Favou/FavRequest.php
@@ -8,19 +8,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class FavRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
+
     public function rules(): array
     {
         return [
