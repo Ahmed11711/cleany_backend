860682449d3180765d5741da6ecde438a359abe7
Ahmed11711 <ahmed11711@gmail.com>
2026-03-30 23:19:44 +0200

    ss

--- app/Http/Requests/Api/Favou/FavRequest.php
+++ app/Http/Requests/Api/Favou/FavRequest.php
@@ -13,7 +13,7 @@ class FavRequest extends BaseRequest
     public function rules(): array
     {
         return [
-            'id' => 'required',
+            'company_id' => 'required',
         ];
     }
 }
