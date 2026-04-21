e1f58451058c15394fcd1ba8946cb007a2eb939a
Ahmed11711 <ahmed11711@gmail.com>
2026-04-21 04:01:23 +0200

    sss

--- app/Models/ServiceItem.php
+++ app/Models/ServiceItem.php
@@ -3,17 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ServiceItem extends Model
 {
     public function driverServices(): HasMany
     {
-        // لاحظ أننا نستخدم 'service_item_id' كـ Foreign Key بناءً على تعديل المايجريشن السابق
         return $this->hasMany(DriverService::class, 'service_item_id');
     }
 
     /**
-     * علاقة عكسية مع الخدمة الرئيسية (إذا كنت تحتاجها)
      */
     public function service()
     {
